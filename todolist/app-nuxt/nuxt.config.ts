1c9cfbe91c0ac30a9fcf3e33cea6e22a1de31911
fatemeh <101033879+fatemehBabaei@users.noreply.github.com>
2022-12-26 12:52:45 +0330

    .

--- todolist/app-nuxt/nuxt.config.ts
+++ todolist/app-nuxt/nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
         "~/assets/styles/master.scss",
     ],
     app: {
-        baseURL: "/todolist-nuxt/todolist/app-nuxt/dist/",
+        baseURL: "/todolist/app-nuxt/dist/",
     },
     build: {
         transpile: ["vuetify"],
